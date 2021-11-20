0089199e73a4cbf20f864ba64ef8c8f0e0428a6b
azxs120 <hazi120@walla.com>
2021-11-20 16:25:44 +0200

    the world
    added the plans and the end platform

---
+++ HW -4/Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offsrt;
+    public float smothSpeed = 0.125f;
+
+    private void Start()
+    {
+        offsrt = transform.position - target.transform.position;
+    }
+
+    //runs right after update
+    void LateUpdate()
+    {
+        Vector3 newPos = target.transform.position + offsrt;
+
+        transform.position = newPos;
+    }
+}
---
+++ HW -4/Assets/MoveBall.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveBall : MonoBehaviour
+{
+    Rigidbody rb;
+    public int ballSpeed;
+    public GameObject winText;
+    bool jump = false;
+    float horizontalMove = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && jump == false)
+        {
+            rb.AddForce(Vector3.up * 850);
+            jump = true;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene("level 2");
+
+        float hMove = Input.GetAxis("Horizontal");
+        float vMove = Input.GetAxis("Vertical");
+
+        Vector3 ballMove = new Vector3(hMove, 0.0f, vMove);
+
+        rb.AddForce(ballMove* ballSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            winText.SetActive(true);
+        }
+        if (collision.gameObject.tag == "Plane")
+            jump = false;
+        
+    }
+}
